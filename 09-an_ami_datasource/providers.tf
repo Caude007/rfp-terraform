e47460378b3597cf1522df1b96a845e436fffd20
Caude007 <147264792+Caude007@users.noreply.github.com>
2026-05-05 08:57:00 +0100

    Update providers.tf

--- 09-an_ami_datasource/providers.tf
+++ 09-an_ami_datasource/providers.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region                  = "us-west-2"
-  shared_credentials_file = "~/.aws/credentials"
+  shared_credentials_files = "~/.aws/credentials"
   profile                 = "vscode"
 }
